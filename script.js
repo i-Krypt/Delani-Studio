bbdaf66f05c5ad6d5d2d3f5856ea34fc08b3ac00
i-Krypt <vamay505@gmail.com>
2019-07-22 03:47:21 +0300

    added toggle function in what we do section

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#design,#design-hide").click(function(){
+    $("#design, #design-hide").toggle();
+    });
+    
+    $("#dev,#dev-hide").click(function(){
+      $("#dev, #dev-hide").toggle();
+      });
+    
+      $("#prod,#prod-hide").click(function(){
+        $("#prod, #prod-hide").toggle();
+        prod
+    });
+    
+  
+    //   $("form").submit(function(){
+    //     var name = document.getElementById('name').value;
+    //     var email = document.getElementById('email').value;
+    //     var message = document.getElementById('textarea').value;
+    //     if (name !=="" && email !=="" && message !==""){
+    //       alert("Thank you" + " "+ name + " "+ "we have received your message. Thank you for reaching out to us. ");
+    //     }else{
+    //       alert("Insert your name, email and message");
+    //     }
+    //     event.preventDefault();
+        
+    //   });
+    // });
+    
